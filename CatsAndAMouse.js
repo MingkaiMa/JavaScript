05655606ddc927cf388ae36b9a01553a2cd04541
Mingkai Ma <mamingkai824@gmail.com>
2018-07-24 16:41:14 +1000

    HackerRank Cats and a Mouse

---
+++ CatsAndAMouse.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the catAndMouse function below.
+function catAndMouse(x, y, z) {
+    if(x == y)
+        return "Mouse C";
+    if((x + y) / 2 == z) {
+        return "Mouse C";
+    }
+    
+
+    
+    var diff_x_z = Math.abs(x - z);
+    var diff_y_z = Math.abs(y - z);
+    
+    if(diff_x_z > diff_y_z) {
+        return "Cat B";
+    }
+    else {
+        return "Cat A";
+    }
+    
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const xyz = readLine().split(' ');
+
+        const x = parseInt(xyz[0], 10);
+
+        const y = parseInt(xyz[1], 10);
+
+        const z = parseInt(xyz[2], 10);
+
+        let result = catAndMouse(x, y, z);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
+
+
